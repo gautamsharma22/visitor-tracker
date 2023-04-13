63385b350254eb24767581b3f3998a938692cb84
gautam <gautamsharma.best@gmail.com>
2023-04-14 00:47:45 +0530

    Inititalisation Done :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
+import React from "react";
+import TestFile from "./Components/TestFile.jsx";
 const App = () => {
   return (
     <div>
-      
+      <TestFile />
     </div>
   );
-}
+};
 
 export default App;
-
--- src/Components/TestFile.jsx
+++ src/Components/TestFile.jsx
@@ -1,7 +1,31 @@
+import React from "react";
 function TestFile() {
+  const users = ["Admin", "Faculty", "Supervisor"];
+  function clickHandler() {
+    console.log("clicked");
+  }
   return (
     <>
-      <h1>Hello From React Revisor...</h1>
+      <div className="dropdown">
+        <button
+          className="btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Select User :
+        </button>
+        <ul className="dropdown-menu">
+          {users.map((user) => (
+            <li>
+              <a className="dropdown-item" onClick={clickHandler}>
+                {user}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
+export default TestFile;
