994a43f98af3a33f57893e81dc337b38e0045145
gautam <gautamsharma.best@gmail.com>
2023-05-02 16:17:43 +0530

    Updated Global Strings >>

--- backend/dbtest.js
+++ backend/dbtest.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const registerUser = require("./routes/user");
 const app = express();
 const cors = require("cors");
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 const connectionURI =
-  "mongodb+srv://root:root@cluster0.s1a1yrw.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s1a1yrw.mongodb.net/?retryWrites=true&w=majority`;
 mongoose
   .connect(connectionURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Database connected! You're Good to go.");
   })
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
 app.use("/users", registerUser);
-app.listen(5000, () => console.log("Server listening on port 5000"));
+app.listen(process.env.LISTEN_PORT, () => console.log(`Server listening on port ${process.env.LISTEN_PORT}`));
