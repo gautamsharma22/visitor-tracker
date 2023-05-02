d71f4d067c4d31b491e53cc2b2d4f9bb28481059
gautam <gautamsharma.best@gmail.com>
2023-05-02 15:00:51 +0530

    Login Working :)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,29 @@ const express = require("express");
 const Visitor = require("../models/visitor");
 const router = express.Router();
 
+router.get("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await Visitor.findOne({ email });
+  if (!existingUser) {
+    return res
+      .status(400)
+      .send({ error: "An account with this email not found. Try Signing Up." });
+  }
+
+  try {
+    const user = await Visitor.findOne({ email: email, password: password });
+    if (user) {
+      res.send({ success: "Logged in succesfully" });
+    } else {
+      res.send({ success: "Credentials don't match, Please try again" });
+    }
+  } catch (error) {
+    console.error("Error Logging in :( ", error);
+    res.status(500).send(error);
+  }
+});
+
 router.post("/register", async (req, res) => {
   const { firstName, lastName, email, reason, password, visitortype } =
     req.body;
@@ -13,6 +36,13 @@ router.post("/register", async (req, res) => {
     password,
     visitortype,
   });
+
+  const existingUser = await Visitor.findOne({ email });
+  if (existingUser) {
+    return res
+      .status(400)
+      .send({ error: "User with this email already exists" });
+  }
 
   try {
     const result = await visitor.save();
