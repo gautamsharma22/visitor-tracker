67f77982d6d9dcc8e95f2410479d039c0896db94
gautam <gautamsharma.best@gmail.com>
2023-11-08 23:58:04 +0530

    app debug

--- backend/app.js
+++ backend/app.js
@@ -8,26 +8,26 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 dotenv.config();
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Expose-Headers", "set-cookie");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  next();
-});
-const connectionURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s1a1yrw.mongodb.net/?retryWrites=true&w=majority`;
-mongoose
-  .connect(connectionURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Database connected! You're Good to go.");
-  })
-  .catch((err) => console.error("Could not connect to MongoDB", err));
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Expose-Headers", "set-cookie");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+//   next();
+// });
+// const connectionURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s1a1yrw.mongodb.net/?retryWrites=true&w=majority`;
+// mongoose
+//   .connect(connectionURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log("Database connected! You're Good to go.");
+//   })
+//   .catch((err) => console.error("Could not connect to MongoDB", err));
 app.use("/", (req, res) => {
   res.send(`app rendered on ${process.env.LISTEN_PORT}`);
 });
-app.use("/users", registerUser);
-app.use("/request", manageRequest);
+// app.use("/users", registerUser);
+// app.use("/request", manageRequest);
 app.listen(process.env.LISTEN_PORT, () =>
   console.log(`Server listening on port ${process.env.LISTEN_PORT}`)
 );
